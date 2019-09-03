b959fe352a4248cd7ddd76638e50dada83f1b493
JunYoung <kimjy4831@naver.com>
2019-09-03 19:56:45 +0900

    jpg bmp png 파일 로딩 추가

--- ImageToOCR.py
+++ ImageToOCR.py
@@ -3,15 +3,23 @@ from pytesseract import *
 import glob
 from PIL import Image
 
-def main():
-    multiImage = glob.glob('*.jpg')
-    for i in multiImage:
-        print(i)
-        image = Image.open(i)
-        text = image_to_string(image, lang="kor+eng")
-        file = open(i+'.txt', 'w', encoding='utf8')
-        file.write(text)
-        file.close()
+def getOCR(multiImage):
+    if multiImage:
+        for i in multiImage:
+            print("Image 로딩중 : " + i)
+            image = Image.open(i)
+            text = image_to_string(image, lang="kor+eng")
+            file = open(i + '.txt', 'w', encoding='utf8')
+            file.write(text)
+            file.close()
 
+def main():
+    jpgImage = glob.glob('*.jpg')
+    bmpImage = glob.glob('*.bmp')
+    pngImage = glob.glob('*.png')
+    getOCR(jpgImage)
+    getOCR(bmpImage)
+    getOCR(pngImage)
+    print("OCR 변환 완료")
 if __name__ == "__main__":
     main()
